3cddf7c884e32206b4d60fd6bd6bef19c128aad2
pptasinski <przemyslaw.ptasinski@gmail.com>
2023-09-06 10:08:39 +0200

    [sylius-days] add on sale flag to product entity

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -7,12 +7,20 @@ namespace App\Entity\Product;
 use Doctrine\ORM\Mapping as ORM;
 use Sylius\Component\Core\Model\Product as BaseProduct;
 
-/**
- * @ORM\Entity
- * @ORM\Table(name="sylius_product")
- */
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_product')]
-class Product extends BaseProduct
+class Product extends BaseProduct implements ProductInterface
 {
+    #[ORM\Column(name: 'is_on_sale', type: 'boolean')]
+    protected bool $isOnSale = false;
+
+    public function isOnSale(): bool
+    {
+        return $this->isOnSale;
+    }
+
+    public function setIsOnSale(bool $isOnSale): void
+    {
+        $this->isOnSale = $isOnSale;
+    }
 }
